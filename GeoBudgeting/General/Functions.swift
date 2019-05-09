9573ad75c84bb906c9304da773a8887318d201cf
Marie Kristein-Harmsen <mharmsen.dvt@gmail.com>
2019-05-09 11:08:27 +0200

    API files and functions changed Categories added

--- GeoBudgeting/General/Functions.swift
+++ GeoBudgeting/General/Functions.swift
@@ -11,13 +11,25 @@ import UIKit
 
 struct API: Codable {
     let GoogleMapsAPI: String!
+    let OCRWebService: OCRWebService!
     
     enum CodingKeys: String, CodingKey {
         case GoogleMapsAPI = "GoogleMapsAPI"
+        case OCRWebService = "OCRWebService"
+    }
+    
+    struct OCRWebService: Codable {
+        let username: String!
+        let APIKey: String!
+        
+        enum CodingKeys: String, CodingKey {
+            case username = "username"
+            case APIKey = "APIKey"
+        }
     }
 }
 
-func getAPIKey() -> String {
+func getGoogleAPIKey() -> String {
     if  let path = Bundle.main.path(forResource: "api", ofType: "plist"),
         let xml = FileManager.default.contents(atPath: path),
         let key = try? PropertyListDecoder().decode(API.self, from: xml) {
@@ -26,6 +38,14 @@ func getAPIKey() -> String {
     return ""
 }
 
+func getOCRKey() -> (String, String) {
+    if  let path = Bundle.main.path(forResource: "api", ofType: "plist"),
+        let xml = FileManager.default.contents(atPath: path),
+        let key = try? PropertyListDecoder().decode(API.self, from: xml) {
+        return (key.OCRWebService.username, key.OCRWebService.APIKey)
+    }
+    return ("","")
+}
 func addTabs(tabIndex: Int, tabController: UITabBarController) {
     switch tabIndex {
     case 0:
